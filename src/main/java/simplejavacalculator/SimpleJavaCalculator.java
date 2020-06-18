ff1d1353816955d413c85806ffccf3a1f7686b0b
Murat Baktur <muratbaktur@gmail.com>
2020-06-18 22:23:30 +0600

    Seventh modify

--- src/main/java/simplejavacalculator/SimpleJavaCalculator.java
+++ src/main/java/simplejavacalculator/SimpleJavaCalculator.java
@@ -1,22 +1,20 @@
-/**
+/*
  * @name        Simple Java Calculator
  * @file        SimpleJavaCalculator.java
- * @author      SORIA Pierre-Henry
- * @copyright   Copyright Pierre-Henry SORIA, All Rights Reserved.
- * @license     Apache (http://www.apache.org/licenses/LICENSE-2.0)
+ * @author      Soria Pierre-Henry
+ * @copyright   Copyright Pierre-Henry Soria, All Rights Reserved.
+ * @license     Apache (http://www.apache.org/licenses/LICENSE-2.0).
  */
 
 package simplejavacalculator;
 
 public class SimpleJavaCalculator {
-   
    public static void main(final String[] args) {
       try {
          UI uiCal = new UI();
          uiCal.init();
       } catch (Exception e) {
-         System.out.println(e.getMessage());   
+         System.out.println(e.getMessage());
       }
-      
    }
 }
