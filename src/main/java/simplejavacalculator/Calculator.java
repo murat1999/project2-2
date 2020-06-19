246e5fcf2b7c7f3cc8c97d925c6bb2d0c39000f6
Murat Baktur <muratbaktur@gmail.com>
2020-06-19 17:18:36 +0600

    34

--- src/main/java/simplejavacalculator/Calculator.java
+++ src/main/java/simplejavacalculator/Calculator.java
@@ -62,7 +62,7 @@ public final class Calculator {
         throw new Error();
     }
 
-    /** @return newMode.*/
+    /** @param newMode .*/
     public Double calculateBi(final BiOperatorModes newMode, final Double num) {
         if (mode == BiOperatorModes.normal) {
             num2 = 0.0;
