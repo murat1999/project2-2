00716e501bd1d273d980d42b1662c1676d84347a
Murat Baktur <muratbaktur@gmail.com>
2020-06-19 15:05:19 +0600

    20

--- src/main/java/simplejavacalculator/UI.java
+++ src/main/java/simplejavacalculator/UI.java
@@ -35,9 +35,24 @@ public class UI implements ActionListener {
    private final JPanel panelSub7;
    private final JPanel panelSub8;
    private final JTextArea text;
-   private final JButton but[], butAdd, butMinus, butMultiply, butDivide,
-      butEqual, butCancel, butSquareRoot, butSquare, butOneDevidedBy,
-      butCos, butSin, butTan, butxpowerofy, butlog, butrate, butabs, butBinary;
+   private final JButton[] but;
+   private final JButton butAdd;
+   private final JButton butMinus;
+   private final JButton butMultiply;
+   private final JButton butDivide;
+   private final JButton butEqual;
+   private final JButton butCancel;
+   private final JButton butSquareRoot;
+   private final JButton butSquare;
+   private final JButton butOneDevidedBy;
+   private final JButton butCos;
+   private final JButton butSin;
+   private final JButton butTan;
+   private final JButton butxpowerofy;
+   private final JButton butlog;
+   private final JButton butrate;
+   private final JButton butabs;
+   private final JButton butBinary;
    private final Calculator calc;
    private final String[] buttonValue = {"0", "1", "2", "3", "4", "5", "6",
       "7", "8", "9"};
@@ -176,7 +191,7 @@ public class UI implements ActionListener {
       frame.add(panel);
    }
    @Override
-   public void actionPerformed(ActionEvent e) {
+   public void actionPerformed(final ActionEvent e) {
       final Object source = e.getSource();
       for (int i = 0; i < 10; i++) {
          if (source == but[i]) {
